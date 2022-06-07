c4723702ebcf2a75c9c450f53233d82237d8c6eb
unknown <abdooisco24@gmail.com>
2022-06-07 13:35:19 +0200

    server directory created

---
+++ server/controllers/playersController.js
@@ -0,0 +1,8 @@
+/* => Get All Players (sport) 
+      " To retrieve players and display them depending on sport type "
+   => Get Player (:id) 
+      " To get player details viewed "
+   => Edit Player (:id) 
+      " To update player data "
+
+*/
