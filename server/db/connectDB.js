582695d6dafe86da8e3a3e81a209d795e428d602
theMohamedELAgamy <98603281+theMohamedELAgamy@users.noreply.github.com>
2022-06-07 14:57:43 +0200

    first commit

--- server/db/connectDB.js
+++ server/db/connectDB.js
@@ -4,7 +4,11 @@ const connectDB = (url) => {
   return mongoose.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  });
+  }).then(()=>{
+    console.log('database connected ....')
+  }).catch((error)=>{
+    console.log(error.massage)
+  })
 };
 
 module.exports = connectDB;
