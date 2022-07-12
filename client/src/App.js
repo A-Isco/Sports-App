f437e547699e8e271f16dbe14ca6f9735cbc692b
reham15 <rehamnader0123@gmail.com>
2022-07-12 18:28:17 +0200

    added playerslist

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ export default function App(){
       <Routes>
         
       <Route path="/" element={ <PlacesList/> } />
+          <Route path="/players" element={ <PlayersList/> } />
       <Route path="/football/places/:placeId" element={ <PlaceDetails/> } />
       <Route path="/chat" element={<ChatComponent/>}/>
       </Routes>
