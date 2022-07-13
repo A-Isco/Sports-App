8d059e946b4c5063963855b303f4b66d0ae97332
DinaAlaa99 <37483521+DinaAlaa99@users.noreply.github.com>
2022-07-13 15:00:57 +0200

    update in places create page

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import PlacesList from "./components/placesList";
 import {Route, Router, Routes} from "react-router";
 import {BrowserRouter} from "react-router-dom";
 import PlaceDetails from "./components/placeDetails";
+import PlacesCreatePage from "./components/PlacesCreatePage";
 
 export default function App() {
     return (
@@ -14,6 +15,7 @@ export default function App() {
 
                     <Route path="/" element={ <PlacesList/> } />
                     <Route path="/football/places/:placeId" element={ <PlaceDetails/> } />
+                    <Route path="/football/create-place" element={ <PlacesCreatePage/> } />
                 </Routes>
             </BrowserRouter>
         </div>
--- client/src/components/PlacesCreatePage.jsx
+++ client/src/components/PlacesCreatePage.jsx
@@ -1,43 +1,37 @@
 // import React from "@types/react";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Select from "react-select";
+//import Select from "react-select";
 import { useNavigate } from "react-router-dom";
 
-let DeveloperCreatePage = () => {
+let PlaceCreatePage = () => {
   let navigate = useNavigate();
   const [Place, setPlace] = useState({
     name: "",
-    Description: "",
-    password: "",
-    password_confirm: "",
-    cv: null,
-    gender: "",
-    date_of_birth: "",
-    tags: [],
-    user_type: "developer",
-    profile_picture: null,
+    description: "",
+    sport:"",
+    price: "",
+    // rate: "",
+    region: "",
+    address: "",
+    profile: null,
   });
-  const [tags, SetTags] = useState([]);
+  const [images, SetImages] = useState([]);
   const [errors, SetErrors] = useState({});
 
-  function createDeveloper(event) {
+  function createPlace(event) {
     event.preventDefault();
 
-    let baseUrl = "http://127.0.0.1:8000/api/v1/account/signup";
+    let baseUrl = "http://localhost:4000/api/places/";
     const headers = {
       "Content-Type": "multipart/form-data",
     };
     axios
-      .post(
-        baseUrl,
-        { ...Developer, tags: Developer.tags.map((o) => Number(o.value))[0] },
-        { headers }
-      )
+      .post(baseUrl, { ...Place }, { headers })
       .then((response) => {
         SetErrors({});
         console.log(response);
-        navigate("/login");
+        //navigate("/places");
         SetErrors({});
       })
       .catch((response) => {
@@ -45,75 +39,77 @@ let DeveloperCreatePage = () => {
         SetErrors(response.response.data);
       });
 
-    console.log(Developer);
+    console.log(Place);
   }
 
-  useEffect(() => {
-    let token = window.localStorage.getItem("token");
-    const headers = {
-      "Content-Type": "application/json",
-    };
-    axios.get("http://127.0.0.1:8000/api/v1/tags", { headers }).then((res) => {
-      console.log(res.data);
-      let received_tags = [];
-      received_tags = res.data.map((item) => {
-        return {
-          label: item.name,
-          value: item.id,
-        };
-      });
+  // useEffect(() => {
+  //   let token = window.localStorage.getItem("token");
+  //   const headers = {
+  //     "Content-Type": "application/json",
+  //   };
+  //axios.get("http://127.0.0.1:8000/api/v1/tags", { headers }).then((res) => {
+  //console.log(res.data);
+  //let received_tags = [];
+  //received_tags = res.data.map((item) => {
+  //       return {
+  //         label: item.name,
+  //         value: item.id,
+  //       };
+  //     });
 
-      SetTags(received_tags);
-    });
-  }, []);
+  //     SetTags(received_tags);
+  //   });
+  // }, []);
 
   const handleChange = (event) => {
-    if (event.target.name !== "cv" && event.target.name !== "profile_picture")
-      setDeveloper({ ...Developer, [event.target.name]: event.target.value });
+    if (event.target.name !== "images")
+      setPlace({ ...Place, [event.target.name]: event.target.value });
     else {
-      if (event.target.name === "cv") {
-        let dev = { ...Developer };
-        dev.cv = event.target.files[0];
-        setDeveloper(dev);
+      if (event.target.name === "profile") {
+        let place = { ...Place };
+        place.profile = event.target.files[0];
+        setPlace(place);
       } else {
-        let dev = Developer;
-        dev.profile_picture = event.target.files[0];
-        setDeveloper(dev);
+        let place = Place;
+        place.images[0] = event.target.files[0];
+        setPlace(place);
       }
     }
-    console.log(Developer);
+    console.log(Place);
   };
 
-  const handleChangeTags = (options) => {
-    setDeveloper((prevState) => ({ ...prevState, tags: options }));
-  };
+  // const handleChangeTags = (options) => {
+  //   setPlace((prevState) => ({ ...prevState }));
+  // };
 
   return (
     <div className="  row">
-      <form className="col-4 mx-auto" onSubmit={(e) => createDeveloper(e)}>
+      <form className="col-4 mx-auto" onSubmit={(e) => createPlace(e)}>
         <div className="text-center p-3">
-          <h3>SIGN UP</h3>
+          <h3>Sports Club</h3>
         </div>
         <div className="form-group mb-2">
-          <label>User Name</label>
+          <label>Name</label>
           <input
             type="text"
             className="form-control"
-            placeholder="Enter user name"
-            name="username"
+            placeholder="Enter Name"
+            name="name"
+            // value={Place.name}
             onChange={(e) => {
               handleChange(e);
             }}
           />
         </div>
         <div className="form-group mb-2">
-          <label>Email address</label>
-          <input
-            type="email"
+          <label>Description</label>
+          <textarea
+            type="textarea"
+            row=""
             className="form-control"
-            placeholder="Enter email"
-            name="email"
-            value={Developer.email}
+            placeholder="Enter Descrition"
+            name="description"
+            // value={Place.description}
             onChange={(e) => {
               handleChange(e);
             }}
@@ -121,98 +117,82 @@ let DeveloperCreatePage = () => {
         </div>
 
         <div className="form-group mb-2">
-          <label>Password</label>
+          <label>Sport</label>
           <input
-            type="password"
+            type="text"
             className="form-control"
-            placeholder="Enter password"
-            name="password"
-            value={Developer.password}
+            placeholder="Enter Sport"
+            name="sport"
+            // value={Place.sport}
             onChange={(e) => {
               handleChange(e);
             }}
           />
         </div>
         <div className="form-group mb-2">
-          <label> repeat Password</label>
+          <label>Price</label>
           <input
-            type="password"
+            type="number"
             className="form-control"
-            placeholder="Enter password"
-            name="password_confirm"
-            value={Developer.password_confirm}
+            placeholder="Enter Price"
+            name="price"
+            // value={Place.sport}
+            onChange={(e) => {
+              handleChange(e);
+            }}
+          />
+        </div>
+        <div className="form-group mb-2">
+          <label> Region</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter region"
+            name="region"
+            // value={Place.region}
+            onChange={(e) => {
+              handleChange(e);
+            }}
+          />
+        </div>
+        <div className="form-group mb-2">
+          <label> Address</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="street,city"
+            name="address"
+            // value={Place.address}
             onChange={(e) => {
               handleChange(e);
             }}
           />
         </div>
 
-        <div className="form-group">
-          <input
-            className="form-check-input"
-            type="radio"
-            name="gender"
-            id="flexRadioDefault1"
-            value="M"
-            onChange={(e) => {
-              handleChange(e);
-            }}
-          />
-          <label className="custom-control-label" htmlFor="customCheck1">
-            male
-          </label>
-        </div>
-        <div className="form-check">
-          <input
-            className="form-check-input"
-            type="radio"
-            name="gender"
-            id="flexRadioDefault2"
-            value="F"
-            onChange={(e) => {
-              handleChange(e);
-            }}
-          />
-          <label className="custom-control-label" htmlFor="customCheck1">
-            female
-          </label>
-        </div>
-
         <div className="form-group mb-2">
-          <label>birth date</label>
-          <input
-            type="date"
-            className="form-control"
-            name="date_of_birth"
-            onChange={(e) => {
-              handleChange(e);
-            }}
-          />
-        </div>
-        <div className="form-group mb-2">
-          <label>upload resume</label>
+          <label>upload photo</label>
           <input
             type="file"
             className="form-control"
-            name="cv"
+            name="profile"
             onChange={(e) => {
               handleChange(e);
             }}
           />
         </div>
         <div className="form-group mb-2">
-          <label>upload Image</label>
+          <label>Images</label>
           <input
             type="file"
             className="form-control"
-            name="profile_picture"
+            name="images"
             onChange={(e) => {
               handleChange(e);
             }}
           />
         </div>
 
-        <div className="form-group mb-2">
+        {/* <div className="form-group mb-2">
           <label>skills</label>
           <Select
             options={tags}
@@ -220,13 +200,13 @@ let DeveloperCreatePage = () => {
             onChange={(e) => {
               handleChangeTags(e);
             }}
-            value={Developer.tags}
+            value={Place.tags}
             name="tags"
           />
-        </div>
+        </div> */}
 
         <button type="submit" className="btn btn-primary btn-block mb-2">
-          SIGN UP
+          Add
         </button>
       </form>
       <div className="mt-2 ">
@@ -244,4 +224,4 @@ let DeveloperCreatePage = () => {
   );
 };
 
-export default DeveloperCreatePage;
+export default PlaceCreatePage;
