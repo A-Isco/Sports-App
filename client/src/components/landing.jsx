fff67051480a4f5b84e1dfe8d43adb181b5b6c6e
theMohamedELAgamy <98603281+theMohamedELAgamy@users.noreply.github.com>
2022-07-23 17:13:00 +0200

    calender edited

--- client/src/components/landing.jsx
+++ client/src/components/landing.jsx
@@ -5,7 +5,7 @@ import PlayersList from "./playersList";
 import Nav from "./core/newHomeBar";
 import LandingNavbar from "./core/landingNavBar";
 import PlaceDetails from "./placeDetails";
-import ChatComponent from "./chat/chatcomponent";
+import CalenderComponent from "./calender/calendercomponent";
 import PlacesCreatePage from "./PlacesCreatePage";
 import PlacesEditPage from "./placesEditPage";
 import AdminRoute from "./auth/adminRoute";
@@ -24,11 +24,11 @@ let Landing = ()=>{
                 <Route path="places" element={<PrivateRoute component={PlacesList}/>} />
                 <Route path="players" element={<PrivateRoute component={PlayersList}/>} />
                 <Route path="places/:placeId" element={<PrivateRoute component={PlaceDetails}/>} />
-                {/*<Route path="/chat"   element={<PrivateRoute component={ChatComponent}/>}/>*/}
+                {/* <Route path="/calender"   element={<PrivateRoute component={CalenderComponent}/>}/> */}
                 <Route path="create-place"  element={<AdminRoute component={PlacesCreatePage}/>}/>
                 < Route path = ":placeId/edit-place" element={<AdminRoute component={PlacesEditPage}/>}/>
                 <Route path='/notfound'element={<NotFound/>}/>
-                />
+                
             </Routes>
 
         </div>
